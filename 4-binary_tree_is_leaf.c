c9aec3a93d01c01e1d49f0d941b141daf5afd091
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-16 11:42:35 -0800

    4-binare_fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,11 +7,12 @@
 **/
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if(node == NULL)
-    return 0;
-  if(node->left == NULL && node->right==NULL)
-    return 1;
-  else
-    return binary_tree_is_leaf(node->left)+
-           binary_tree_is_leaf(node->right);
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return binary_tree_is_leaf(node->left) +
+		binary_tree_is_leaf(node->right);
 }
