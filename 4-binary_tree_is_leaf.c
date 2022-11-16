92824777be9837cbd0c9780cfd0f64cb0b311615
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-16 11:19:25 -0800

    4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - check code
+* @node: is a pointer to the node to check
+* Return: 1 if node is a leaf, otherwise 0
+**/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == null)
+		return (0);
+
+	if (node->left == null && node->right == null)
+		return (1);
+
+	binary_is_leaf(node->left);
+	binary_is_leaf(node->right);
+}
