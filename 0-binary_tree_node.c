7e4920d7ba53c1559b4c08f4eaa24b3b2d64565f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-16 03:17:22 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,7 +5,7 @@
 * @value: the value of the binary tree
 * Return: a pointer to the new node, or NULL on failure
 **/
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tmp = NULL;
 
