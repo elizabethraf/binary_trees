1940d53f25faf6ce7272a939f7230a5c3c3bf5e2
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-16 11:59:24 -0800

    6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - check code
+* @tree: is a pointer to the root node of the tree to traverse
+* @func: is a pointer to a function to call for each node
+* If tree or func is NULL, do nothing
+**/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	for (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
