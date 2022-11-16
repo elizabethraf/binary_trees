166e1c474b3dd07753cf8f5da036e24ae4084b1a
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-16 09:42:18 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,16 +9,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tmp;
 
-	tmp = (node *)malloc(sizeof(node));
-
+	tmp = malloc(sizeof(binary_tree_t));
 	if (tmp == NULL)
-			return (NULL)
 	{
-		tmp->n = value;
-		tmp->parent = parent;
-		tmp->left = tmp->right = NULL;
-		*binary_tree = tmp;
+		return (NULL);
 	}
+	tmp->n = value;
+	tmp->parent = parent;
+	tmp->left = NULL;
+	tmp->right = NULL;
 
-		return (temp);
+	return (tmp);
 }
