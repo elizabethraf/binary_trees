e39a074fbde2e7fc29cab6f0143ec21a26bcea2b
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-16 21:30:48 -0800

    9-binary_fix

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_depth - check code
+* @tree: is a pointer to the node to measure the depth
+* Return: If tree is NULL, your function must return 0
+**/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree);
 
 	else
 	{
-		left_height = tree_height(tree->left);
-		right_height = tree_height(tree->right);
+		1 = left_height ? 1 + binary_tree_height(tree->left) : 0;
+		1 = right_height ? 1 + binart_tree_height(tree->right) : 0;
 
 		return (max(left_height, right_height) + 1);
 	}
