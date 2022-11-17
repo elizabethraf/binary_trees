4e3ce4bde7ab0d2d7084101360f1e9e9ba9532c1
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-17 11:26:41 -0800

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+* binary_trees_ancestor - check code
+* @first: is a pointer to the first node
+* @second: is a pointer to the second node
+* Return: must return NULL
+**/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *prev, *tmp;
+
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	prev = first->parent;
+	tmp = second->parent;
+	if (prev == NULL || first == tmp || (!prev->parent && tmp))
+	{
+		return (binary_trees_ancestor(first, tmp));
+	}
+	else
+		if (tmp == NULL || prev == second || (!tmp->parent && prev))
+			return (binary_trees_ancestor(prev, second));
+
+	return (binary_trees_ancestor(prev, tmp));
+}
