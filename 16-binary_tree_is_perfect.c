078acfa24b6be58312644dcdf9e7a259beeb8390
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-17 10:09:32 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,20 +11,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree->left && tree->right)
 	{
-		left = 1 + tree_is_perfect(tree->left);
-		right = 1 + tree_is_perfect(tree->right);
+		left = 1 + binary_tree_is_perfect(tree->left);
+		right = 1 + binary_tree_is_perfect(tree->right);
 		if (right == left && right != 0 && left != 0)
 			return (right);
 		return (0);
 	}
 	else
 		if (!tree->left && !tree->right)
-		{
 			return (1);
-		}
-}
 
-#include "binary_trees.h"
+}
 
 /**
 * binary_tree_height - check code
@@ -42,4 +39,26 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		height += (r > l ? r : l);
 	}
 	return (height);
+}
+
+/**
+* binary_tree_depth - check code
+* @tree: is a pointer to the node to measure the depth
+* Return: If tree is NULL, your function must return 0
+**/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	{
+		size_t ldepth = binary_tree_depth(tree->left);
+		size_t rdepth = binary_tree_depth(tree->right);
+
+		if (ldepth > rdepth)
+			return (ldepth + 1);
+
+		else
+			return (rdepth + 1);
+	}
 }
